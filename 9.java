4948020e8f57528b590fe09756948c2d727ede24
Vaghela Madhuri <madhurivaghela463@gmail.com>
2025-09-18 16:03:07 +0530

    9.java

---
+++ 9.java
@@ -0,0 +1,21 @@
+//package ✓
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in) ;
+		int n = sc.nextInt();
+
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= i; j++) {
+				int sum = i + j ;
+				if (sum % 2 == 0) {
+					System.out.print("0" + " ");
+				} else {
+					System.out.print("1" + " ");
+				}
+			}
+			System.out.println();
+
+		}
+	}
+}
